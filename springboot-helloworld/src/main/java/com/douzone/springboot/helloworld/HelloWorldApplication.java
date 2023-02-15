4045c4eb710dc49b80c18862bf423d96f8d3c070
yoon-ji-kim <yoonjigim0.0@gmail.com>
2023-02-15 14:05:48 +0900

    springboot-helloworld

---
+++ springboot-helloworld/src/main/java/com/douzone/springboot/helloworld/HelloWorldApplication.java
@@ -0,0 +1,29 @@
+package com.douzone.springboot.helloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/*
+ *  1. 스프링 애플리케이션의 부트스트래핑 역할을 하는 클래스 : Bootstrapping Class
+ *  2. 설정 클래스 역할 : Configuration Class
+ *  
+ */
+@SpringBootApplication
+public class HelloWorldApplication {
+	
+	public static void main(String[] args) {
+		/*
+		 * SpinrgApplication.run(...) 안에서 일어나는 일(BootStrap)
+		 * 
+		 *  1. 애플리케이션 컨텍스트(Application Context, Spring Container) 생성
+		 *  2. 만약, 웹 애플리케이션이라면 웹어플리케이션 타입을 결정(SpringMVC 또는 Reactive)
+		 *  3. 어노테이션 스캐닝(Auto) + Configuration Class를 통한
+		 *  	빈 생성, 등록, 와이어링 작업
+		 *  4. 만약 웹 애플리케이션이고 type이 Spring Mvc 이면
+		 *  - 내장(embedd) 서버(TomcatEmbededServiceServletContainer)에 웹 애플리케이션 배포
+		 *  5. ApplicationRunner 인터페이스를 구현한 빈을 Application Context에서 찾아서 실행
+		 */
+		SpringApplication.run(HelloWorldApplication.class, args);
+	}
+
+}
